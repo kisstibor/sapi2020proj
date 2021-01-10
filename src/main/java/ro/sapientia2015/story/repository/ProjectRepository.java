920accc31477202ac575068c32c3265849827ded
gergelysz <gszblcs97@gmail.com>
2021-01-10 16:31:44 +0200

    Removed commented old classes

--- src/main/java/ro/sapientia2015/story/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-//package ro.sapientia2015.story.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import ro.sapientia2015.story.model.Project;
-//
-//public interface ProjectRepository extends JpaRepository<Project, Long> {
-//}
