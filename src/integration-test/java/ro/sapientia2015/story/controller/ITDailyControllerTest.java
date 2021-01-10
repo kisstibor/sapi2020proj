06b885bc17f3730f90041da52ebd05fef7b1f102
Dorottya <macidori98@gmail.com>
2021-01-10 14:56:44 +0200

    ITDailyControllerTest started

---
+++ src/integration-test/java/ro/sapientia2015/story/controller/ITDailyControllerTest.java
@@ -0,0 +1,73 @@
+package ro.sapientia2015.story.controller;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+
+import ro.sapientia2015.config.ExampleApplicationContext;
+import ro.sapientia2015.context.WebContextLoader;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = WebContextLoader.class, classes = {ExampleApplicationContext.class})
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
+@DatabaseSetup("dailyData.xml")
+public class ITDailyControllerTest {
+	private static final String FORM_FIELD_DESCRIPTION = "description";
+    private static final String FORM_FIELD_ID = "id";
+    private static final String FORM_FIELD_TITLE = "title";
+    private static final String FORM_FIELD_DATEE = "datee";
+    private static final String FORM_FIELD_DURATION = "duration";
+    
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webApplicationContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    @ExpectedDatabase(value = "dailyData.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void showForm() throws Exception {
+    	mockMvc.perform(get("/daily/add"))	
+    	.andExpect(status().isOk());
+    }
+
+}
