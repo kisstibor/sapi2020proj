92541ba41cd8f7420beb79399405b4bd5253be70
Tibor <tibor@sapi.ro>
2021-01-11 18:18:27 +0200

    Secound

--- src/main/java/ro/sapientia2015/story/controller/StoryController.java
+++ src/main/java/ro/sapientia2015/story/controller/StoryController.java
@@ -19,9 +19,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Locale;
 
-/**
- * @author Kiss Tibor
- */
+
 @Controller
 @SessionAttributes("story")
 public class StoryController {
